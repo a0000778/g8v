a535bedd8cc91217ab64721bc4500a5ab19bccf5
a0000778 <a0000778@gmail.com>
2014-05-16 03:34:25 +0800

    新增AJAX函式庫以便調用API

--- js/common.js
+++ js/common.js
@@ -37,3 +37,88 @@ HTMLElement.prototype.$attr=function(key, value){
 if(!HTMLCollection.prototype.forEach) HTMLCollection.prototype.forEach=Array.prototype.forEach;
 if(!NamedNodeMap.prototype.forEach) NamedNodeMap.prototype.forEach=Array.prototype.forEach;
 if(!NodeList.prototype.forEach) NodeList.prototype.forEach=Array.prototype.forEach;
+
+function Ajax(method, url, data, option){
+	this.xhr=new XMLHttpRequest();
+	this.method=method.toUpperCase();
+	this.url=url;
+	this.data=data? data:null;
+	var option=option? option:{};
+	this.option={
+		'async': option.hasOwnProperty('async')? option.async:true,
+		'auth': option.hasOwnProperty('auth')? {
+			'user': option.auth.hasOwnProperty('user')? option.auth.user:null,
+			'pass': option.auth.hasOwnProperty('pass')? option.auth.pass:null
+		}:{},
+		'formData': option.hasOwnProperty('formData')? formData:true
+	};
+}
+Ajax.prototype.setHeader=function(header,value){
+	this.xhr.setRequestHeader(header,value);
+	return this;
+}
+Ajax.prototype.on=function(eventName,func){
+	this.xhr.addEventListener(eventName,func.bind(this));
+	return this;
+}
+Ajax.prototype.send=function(){
+	if(this.method=='GET' || !this.option.formData){
+		var data='';
+		var s='';
+		for(k in this.data){
+			if('object'==typeof this.data[k]){
+				data+=s+this.data[k].reduce(function(r,v){
+					r.r+=r.s+r.k+'='+encodeURIComponent(v);
+					r.s='&';
+				},{'r':'','s':'','k':k+'[]'}).r;
+			}else{
+				data+=s+k+'='+encodeURIComponent(this.data[k]);
+			}
+			s='&';
+		}
+	}else{
+		var data=new FormData();
+		for(k in this.data){
+			if('object'==typeof this.data[k]){
+				this.data[k].forEach(function(k,v){
+					this.append(k+'[]',v);
+				}.bind(data,k));
+			}else{
+				data.append(k,this.data[k]);
+			}
+		}
+	}
+	this.xhr.open(
+		this.method,
+		this.url+(this.method=='GET'? (this.url.indexOf('?')>=0? '&':'?')+data:''),
+		this.option.async,
+		this.option.auth.user,
+		this.option.auth.pass
+	);
+	this.xhr.send(this.method=='GET'? null:data);
+	return this;
+}
+Ajax.prototype.abort=function(){
+	this.xhr.abort();
+	return this;
+}
+Ajax.prototype.result=function(forceText){
+	if(this.xhr.readyState!==4) return null;
+	return forceText? this.xhr.responseText:this.xhr.response;
+}
+Ajax.prototype.resultHeader=function(select){
+	if(this.xhr.readyState!==4) return null;
+	if(select) return this.xhr.getResponseHeader(select);
+	else return this.xhr.getAllResponseHeaders().split('\n').reduce(function(r,v){
+		if(!v.length) return r;
+		var s=v.indexOf(':');
+		r[v.substring(0,s)]=v.substring(s+2,v.length);
+		return r;
+	},{});
+}
+function ajaxget(url,data){
+	return new Ajax('GET',url,data,{'async': false}).send().result();
+}
+function ajaxpost(url,data){
+	return new Ajax('POST',url,data,{'async': false}).send().result();
+}
