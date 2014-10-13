f6710772d16232501784114d5a12b8ac528b2dcb
a0000778 <a0000778@gmail.com>
2014-10-14 01:07:17 +0800

    改寫 video 架構以利支援其他協定的來源

--- module/video.js
+++ module/video.js
@@ -1,31 +1,23 @@
 (function(){
 	var video={
 		'source': {
-			'livehouse': function(path){
-				var id=path.match(/^channel\/([a-zA-Z0-9_-]+)/);
-				var record=path.indexOf('/record/');
-				if(record===-1)
-					return $.tag('iframe',{
-						'src': 'https://livehouse.in/embed/channel/'+(id? id[1]:''),
-						'allowfullscreen': 'true',
-						'style': {
-							'width': '100%',
-							'height': '100%'
-						}
-					});
-				else
-					return $.tag('iframe',{
-						'src': 'data:text/html,尚不支援記錄！',
-						'style': {
-							'width': '100%',
-							'height': '100%'
-						}
-					});
+			'livehouse': function(url,unCheckDomain){
+				//var data=url.match(/^(https:\/\/livehouse\.in\/)?channel\/([a-zA-Z0-9_-]+)(?:\/records\/([a-zA-Z0-9_-]{20}))$/);
+				var data=url.match(/^(https:\/\/livehouse\.in\/)?channel\/([a-zA-Z0-9_-]+)$/);
+				if(!data || !(data[1] || unCheckDomain)) return false;
+				return $.tag('iframe',{
+					'src': 'https://livehouse.in/embed/channel/'+data[2],
+					'allowfullscreen': 'true',
+					'style': {
+						'width': '100%',
+						'height': '100%'
+					}
+				});
 			},
-			'twitch': function(path){
-				var id=path.match(/^([a-zA-Z0-9_-]+)/);
-				var recorded=path.match(/\/c\/(\d+)/);
-				if(recorded){
+			'twitch': function(url,unCheckDomain){
+				var data=url.match(/^(http:\/\/(?:www\.)?twitch\.tv\/)?([a-zA-Z0-9_-]+)(?:\/[bc]\/(\d+))/);
+				if(!data || !(data[1] || unCheckDomain)) return false;
+				if(data[3]){
 					var content=$.tag('object',{
 						'type': 'application/x-shockwave-flash',
 						'data': 'http://www.twitch.tv/widgets/archive_embed_player.swf',
@@ -39,12 +31,12 @@
 						<param name="allowScriptAccess" value="always">\
 						<param name="allowNetworking" value="all">\
 						<param name="movie" value="http://www.twitch.tv/widgets/archive_embed_player.swf">\
-						<param name="flashvars" value="channel='+id[1]+'&amp;auto_play=true&amp;chapter_id='+recorded[1]+'">\
+						<param name="flashvars" value="channel='+data[2]+'&amp;auto_play=true&amp;chapter_id='+data[3]+'">\
 					';
 					return content;
 				}else{
 					return $.tag('iframe',{
-						'src': 'http://www.twitch.tv/'+id[1]+'/embed',
+						'src': 'http://www.twitch.tv/'+data[2]+'/embed',
 						'allowfullscreen': 'true',
 						'style': {
 							'width': '100%',
@@ -53,10 +45,12 @@
 					});
 				}
 			},
-			'ustream': function(path){
-				if(path.indexOf('recorded')===0){
+			'ustream': function(url,unCheckDomain){
+				var data=url.match(/^(http:\/\/(?:www.)?ustream.tv\/)?((?:(channel|recorded)\/)?((?:[-+_~.\d\w]|%[a-fA-F\d]{2})+))/);
+				if(!data || !(data[1] || unCheckDomain)) return false;
+				if(data[3]==='recorded'){
 					return $.tag('iframe',{
-						'src': 'http://www.ustream.tv/embed/recorded/'+path.match(/^recorded\/(\d+)/)[1]+'?v=3&wmode=direct&autoplay=1',
+						'src': 'http://www.ustream.tv/embed/recorded/'+data[4]+'?v=3&wmode=direct&autoplay=1',
 						'style': {
 							'width': '100%',
 							'height': '100%'
@@ -71,14 +65,13 @@
 						'background': '#FFF'
 					}
 				});
-				var checkIdOnly=path.match(/^(channel\/)?(\d+)/);
-				if(checkIdOnly){
-					tag.src='http://www.ustream.tv/embed/'+checkIdOnly[2]+'?v=3&wmode=direct&autoplay=1';
+				if(data[3] && !Number.isNaN(parseInt(data[4],10))){
+					tag.src='http://www.ustream.tv/embed/'+data[4]+'?v=3&wmode=direct&autoplay=1';
 					return tag;
 				}
 				new Ajax('POST','http://g8v-a0000778.rhcloud.com/getSourceId',{
 					'source': 'ustream',
-					'path': path.match(/^(channel\/)?([-+_~.\d\w]|%[a-fA-F\d]{2})+/)[0]
+					'path': data[2]
 				}).on('load',function(){
 					tag.src=this.result()? 'http://www.ustream.tv/embed/'+this.result()+'?v=3&wmode=direct&autoplay=1':'data:text/html,Load Failed.';
 				}).on('error',function(){
@@ -86,45 +79,53 @@
 				}).send();
 				return tag;
 			},
-			'youtube': function(path){
-				var id=path.match(/(\?|&)v=([a-zA-Z0-9_-]+)/);
-				var list=path.match(/(\?|&)list=([a-zA-Z0-9_-]+)/);
+			'youtube': function(url,unCheckDomain){
+				if(!(unCheckDomain || /^https:\/\/(?:www.)?youtube.com\/(watch|playlist)/.test(url))) return false;
+				var id=url.match(/(?:\?|&)v=([a-zA-Z0-9_-]+)(?:&|#|$)/);
+				var list=url.match(/(?:\?|&)list=([a-zA-Z0-9_-]+)(?:&|#|$)/);
+				if(!(id || list)) return false;
 				return $.tag('iframe',{
-					'src': 'http://www.youtube.com/embed/'+(id? id[2]:'')+'?rel=0&showinfo=0&autoplay=1'+(list? '&list='+list[2]:''),
+					'src': 'http://www.youtube.com/embed/'+(id? id[1]:'')+'?rel=0&showinfo=0&autoplay=1'+(list? '&list='+list[1]:''),
 					'allowfullscreen': 'true',
 					'style': {
 						'width': '100%',
 						'height': '100%'
 					}
-				})
+				});
 			}
 		},
-		'load': function(source,path,title,left,top,width,height){
-			if(!this.source[source]) return false;
+		'load': function(source,data,title,left,top,width,height){
+			var content;
+			if(!source){
+				for(source in this.source){
+					if(content=this.source[source](data)) break;
+				}
+				if(!content) return false;
+			}else if(!this.source[source]) return false;
 			var obj=g8v.createObj(
 				'video',
-				[source,path],
+				[source,data],
 				title? title:source,
 				left? left:0,
 				top? top:0,
 				width? width:800,
 				height? height:600
 			);
-			g8v.createWindow(obj,title,this.source[source](path));
+			g8v.createWindow(obj,title,content? content:this.source[source](data,true));
 			g8v.updateShareUrl();
 			return obj;
 		}
 	};
 	var form=$.tag('form')
 		.$add(document.createTextNode('新增畫面：'),null,true)
-		.$add('input',{'type':'input','name':'url','placeholder':'輸入直播瀏覽頁網址'},null,true)
+		.$add('input',{'type':'input','name':'url','placeholder':'直播網址'},null,true)
 		.$add('input',{'type':'submit','value':'新增'},null,true)
 	;
 	form.addEventListener('submit',function(e){
 		e.preventDefault();
-		var url=e.target.querySelector('[name=url]').value.match(/^(http(s)?:\/\/)?([a-zA-Z0-9-]+\.)?([a-zA-Z0-9-]+)(\.[a-zA-Z0-9-]+)+\/(.+)/);
+		var url=e.target.querySelector('[name=url]').value;
 		e.target.querySelector('[name=url]').value='';
-		if(video.load(url[4],url[6],url[4])===false) alert('網址格式錯誤或不支援的格式！');
+		if(video.load(undefined,url)===false) alert('網址格式錯誤或不支援的格式！');
 	});
 	g8v.module.config.addItem(form);
 	g8v.module.video=video;
