36c323bf2b540932150de88166a91a1895efca5b
a0000778 <a0000778@gmail.com>
2015-09-24 01:00:36 +0800

    轉移大畫面模組 由複製內容改為放大原視窗

--- module/bigScreen.js
+++ module/bigScreen.js
@@ -1,61 +1,105 @@
-(function(){
-	var appendData={
-		'module': 'bigScreen',
-		'args': [0,0,800,600]
-	}
-	var nowObj=null;
-	var windowObj=$().$add('div',{
-		'className': 'window'
+'use strict';
+/*
+	G8V Big Screen Module by a0000778
+	MIT License
+*/
+{
+	let appendItem;
+	let screenInfo=[0,0,800,600];
+	
+	g8v.loading('bigScreen');
+	g8v.module.set('bigScreen',{
+		'append': append
 	});
-	windowObj.$add('div',{'className':'vw_bar','textContent':'大畫面'});
-	windowObj.$add('ul',{'className':'vw_option'})
-		.$add('li',{'className':'vw_hide ion-close-round','title':'關閉視窗'})
-	;
-	var VW=new VirtualWindow(windowObj,0,0,800,600)
-		.close()
-		.on('close',function(){
-			VW.content.innerHTML='';
-		})
-		.on('move',function(){
-			appendData.args[0]=this.posX;
-			appendData.args[1]=this.posY;
-			g8v.updateShareUrl();
-		})
-		.on('resize',function(){
-			appendData.args[2]=this.width;
-			appendData.args[3]=this.height;
-			g8v.updateShareUrl();
-		})
-	;
-	VW.content=VW.obj.$add('div',{
-		'style': {
-			'width': '100%',
-			'height': '100%'
-		}
-	});
-	g8v.windowOption.push(function(obj){
+	g8v.windowOption.push(function(item){
 		return $.tag('li',{
-			'className': 'ion-ios7-albums',
+			'className': 'ion-ios-albums',
 			'title': '大畫面',
 			'addEventListener': ['click',function(){
-				bigScreen.append(obj);
+				if(appendItem && appendItem.contentItem===item)
+					appendItem.delete();
+				else
+					append(item);
 			}]
 		});
 	});
-	var bigScreen=g8v.module.bigScreen={
-		'append': function(obj,left,top,width,height){
-			if(nowObj)
-				nowObj.append.splice(nowObj.append.indexOf(appendData),1);
-			nowObj=obj;
-			left=left!==undefined? left:appendData.args[0];
-			top=top!==undefined? top:appendData.args[1];
-			width=width!==undefined? width:appendData.args[2];
-			height=height!==undefined? height:appendData.args[3];
-			nowObj.append.push(appendData);
-			VW.content.innerHTML='';
-			VW.content.$add(nowObj.vw.obj.children[2].cloneNode(true));
-			VW.open().moveTo(left,top).resize(width,height).toTop();
-			g8v.updateShareUrl();
-		}
+	g8v.loaded('bigScreen');
+	
+	function BigScreenItem(){
+		g8v.AppendItem.call(this,'bigScreen',[0,0,0,0]);
+		Object.defineProperty(this,'oldPosX',{
+			'get': () => this.args[0],
+			'set': (v) => this.args[0]=v
+		});
+		Object.defineProperty(this,'oldPosY',{
+			'get': () => this.args[1],
+			'set': (v) => this.args[1]=v
+		});
+		Object.defineProperty(this,'oldWidth',{
+			'get': () => this.args[2],
+			'set': (v) => this.args[2]=v
+		});
+		Object.defineProperty(this,'oldHeight',{
+			'get': () => this.args[3],
+			'set': (v) => this.args[3]=v
+		});
+		this.appended=false;
 	}
-})();
+	BigScreenItem.prototype.__proto__=g8v.AppendItem.prototype;
+	BigScreenItem.prototype._append=function(){
+		if(!this.contentItem.vw){
+			console.error('[BigScreen] 錯誤的附加對象');
+			this.delete();
+		}
+		this.oldPosX=this.contentItem.posX;
+		this.oldPosY=this.contentItem.posY;
+		this.oldWidth=this.contentItem.width;
+		this.oldHeight=this.contentItem.height;
+		this.contentItem.vw
+			.moveTo(screenInfo[0],screenInfo[1])
+			.resize(screenInfo[2],screenInfo[3])
+			.on('move',updateScreenPos)
+			.on('resize',updateScreenSize)
+			.on('dragEnd',updateScreenPos)
+			.on('resizeEnd',updateScreenPos)
+			.on('resizeEnd',updateScreenSize)
+		;
+		this.appended=true;
+	}
+	BigScreenItem.prototype._delete=function(){
+		if(!this.appended) return;
+		this.contentItem.vw
+			.unOn('move',updateScreenPos)
+			.unOn('resize',updateScreenSize)
+			.unOn('dragEnd',updateScreenPos)
+			.unOn('resizeEnd',updateScreenPos)
+			.unOn('resizeEnd',updateScreenSize)
+			.moveTo(this.oldPosX,this.oldPosY)
+			.resize(this.oldWidth,this.oldHeight)
+		;
+		this.appended=false;
+	}
+	function append(item,args){
+		if(!appendItem) appendItem=new BigScreenItem();
+		if(appendItem.contentItem)
+			appendItem.delete();
+		if(!item.vw) return;
+		if(args){
+			screenInfo=[item.posX,item.posY,item.width,item.height];
+			appendItem.append(item);
+			appendItem.oldPosX=args[0];
+			appendItem.oldPosY=args[1];
+			appendItem.oldWidth=args[2];
+			appendItem.oldHeight=args[3];
+		}else
+			appendItem.append(item);
+	}
+	function updateScreenPos(){
+		screenInfo[0]=appendItem.contentItem.posX;
+		screenInfo[1]=appendItem.contentItem.posY;
+	}
+	function updateScreenSize(){
+		screenInfo[2]=appendItem.contentItem.width;
+		screenInfo[3]=appendItem.contentItem.height;
+	}
+}
