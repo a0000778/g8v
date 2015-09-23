dfebceb865216c23ae563eda81e94763ed1bd269
a0000778 <a0000778@gmail.com>
2015-09-24 00:56:16 +0800

    轉移透明度模組並提供更好的操作方式

--- module/opacity.js
+++ module/opacity.js
@@ -1,40 +1,74 @@
-(function(){
-	var getAppendData=function(obj){
-		var append=obj.append;
-		var i=-1;
-		var max=append.length
-		while((++i)<max){
-			if(append[i].module==='opacity')
-				return append[i];
-		}
-		var opacityObj={'module': 'opacity','args':[1]}
-		append.push(opacityObj);
-		return opacityObj;
-	}
-	g8v.windowOption.push(function(obj){
-		return $.tag('li',{
+'use strict';
+/*
+	G8V Opacity Module by a0000778
+	MIT License
+*/
+{
+	const levels=[1,0.7,0.4,0.1,0];
+	const levelsReverse=levels.slice().reverse();
+	
+	g8v.loading('opacity');
+	g8v.windowOption.push(function(item){
+		let ele=$.tag('li',{
 			'className': 'ion-contrast',
-			'title': '視窗透明度',
-			'addEventListener': ['click',function(){
-				var opacityData=getAppendData(obj);
-				var opacity=opacityData.args[0];
-				if(opacity>=1) obj.vw.obj.style.opacity=opacityData.args[0]=0.7;
-				else if(opacity>=0.7) obj.vw.obj.style.opacity=opacityData.args[0]=0.4;
-				else if(opacity>=0.4) obj.vw.obj.style.opacity=opacityData.args[0]=0.1;
-				else{
-					obj.vw.obj.style.opacity='1';
-					obj.append.splice(obj.append.indexOf(opacityData),1);
-				}
-				g8v.updateShareUrl();
-			}]
+			'title': '視窗透明度'
 		});
+		ele.addEventListener('click',function(e){
+			e.preventDefault();
+			let opacityItem=getOpacityItem(item);
+			let opacity;
+			for(opacity of levels){
+				if(opacityItem.opacity>opacity){
+					opacityItem.opacity=opacity;
+					break;
+				}
+			}
+			if(opacity) return;
+			else opacityItem.delete();
+		});
+		ele.addEventListener('contextmenu',function(e){
+			e.preventDefault();
+			let opacityItem=getOpacityItem(item);
+			let opacity;
+			for(opacity of levelsReverse){
+				if(opacityItem.opacity<opacity){
+					opacityItem.opacity=opacity;
+					break;
+				}
+			}
+			if(opacity!==1) return;
+			else opacityItem.delete();
+		});
+		return ele;
 	});
-	g8v.module.opacity={
-		'append': function(obj,value){
-			value=parseFloat(value,10);
-			getAppendData(obj).args[0]=value;
-			obj.vw.obj.style.opacity=value;
-			g8v.updateShareUrl();
+	g8v.module.set('opacity',{
+		'append': function(item,args){
+			new OpacityItem(item).opacity=args[0];
 		}
+	});
+	g8v.loaded('opacity');
+	
+	function OpacityItem(item){
+		g8v.AppendItem.call(this,'opacity',[1]);
+		Object.defineProperty(this,'opacity',{
+			'get': () => Number.parseFloat(this.contentItem.content.style.opacity),
+			'set': (value) => {
+				this.contentItem.content.style.opacity=this.args[0]=value;
+				this.emit('updateShareUrl');
+			}
+		});
+		
+		this.append(item);
+		this.opacity=1;
 	}
-})();
+	OpacityItem.prototype.__proto__=g8v.AppendItem.prototype;
+	OpacityItem.prototype._delete=function(){
+		this.opacity=1;
+	}
+	function getOpacityItem(item){
+		let appendItem=item.findAppend('opacity');
+		if(appendItem.length)
+			return appendItem[0];
+		return new OpacityItem(item);
+	}
+}
