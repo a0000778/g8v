d74a5f2035f7af404d8fa78b962a3e6c063c3bd1
a0000778 <a0000778@gmail.com>
2016-04-07 17:08:40 +0800

    livehouse 視窗支援畫面及聊天分開

--- module/chat.js
+++ module/chat.js
@@ -17,7 +17,7 @@
 				var data=url.match(/^(https?:\/\/livehouse\.in\/)?channel\/([a-zA-Z0-9_-]+)/);
 				if(!data || !(data[1] || unCheckDomain)) return false;
 				return $.tag('iframe',{
-					'src': 'https://livehouse.in/embed/channel/'+data[2],
+					'src': 'https://livehouse.in/channel/'+data[2]+'/chatroom',
 					'allowfullscreen': 'true',
 					'style': {
 						'width': '100%',
